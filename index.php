2046f27be3606ce0bcd9efef50118b935d074b59
Kirill Prokudin <kir.prokudin@yandex.ru>
2018-09-13 21:23:11 +0700

    Исправил
    Заменил сравнение с единицей на просто проверку

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $user_avatar = 'img/user.jpg';
         <nav class="user-menu">
 
         <!-- здесь должен быть PHP код для показа аватара пользователя -->
-        <?php if ($is_auth == 1): ?>
+        <?php if ($is_auth): ?>
           <div class="user-menu__image">
             <img src="img/user.jpg" width="40" height="40" alt="Пользователь">
           </div>
