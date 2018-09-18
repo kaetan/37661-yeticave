aadd1c32d91094afc4e21ed799e31ac44b1e297c
Kirill Prokudin <kir.prokudin@yandex.ru>
2018-09-18 13:47:54 +0700

    Поправил цену

--- index.php
+++ index.php
@@ -119,6 +119,8 @@ $user_avatar = 'img/user.jpg';
                       <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$val['Название']; ?></a></h3>
                       <div class="lot__state">
                           <div class="lot__rate">
+                              <span class="lot__amount">Стартовая цена</span>
+                              <span class="lot__cost"><?=$val['Цена']; ?><b class="rub">р</b></span>
                           </div>
                           <div class="lot__timer timer">
 
