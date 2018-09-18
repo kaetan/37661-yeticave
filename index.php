09419b4e240c99c7cb90f38919ce36123832aeec
Kirill Prokudin <kir.prokudin@yandex.ru>
2018-09-18 18:21:50 +0700

    Исправил print

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ function format_cost($cost) {
     $cost = ceil($cost);
     $cost = number_format($cost, 0, ',', ' ');
     $cost = $cost . "<b class=\"rub\">р</b>";
-    print($cost);
+    return($cost);
 }
 ?>
 
@@ -133,7 +133,7 @@ function format_cost($cost) {
                           <div class="lot__rate">
                               <span class="lot__amount">Стартовая цена</span>
                               <span class="lot__cost">
-                                  <?php format_cost($val['cost']); ?>
+                                  <?php print(format_cost($val['cost'])); ?>
                               </span>
                           </div>
                           <div class="lot__timer timer">
