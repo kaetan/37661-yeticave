93bd4901b4b6deaaf2c9b61bf6442fb150081615
Kirill Prokudin <kir.prokudin@yandex.ru>
2018-09-29 19:23:45 +0700

    Исправил структуру и запросы

--- queries.sql
+++ queries.sql
@@ -1,19 +1,19 @@
-INSERT INTO categories(cat_id, cat_title)
+INSERT INTO categories(title)
 VALUES
-(1, 'Доски и лыжи'),
-(2, 'Крепления'),
-(3, 'Ботинки'),
-(4, 'Одежда'),
-(5, 'Инструменты'),
-(6, 'Разное');
+('Доски и лыжи'),
+('Крепления'),
+('Ботинки'),
+('Одежда'),
+('Инструменты'),
+('Разное');
 
-INSERT INTO users(user_id, registration_date, email, username, password, user_pic, contacts, created_lots, placed_bets)
+INSERT INTO users(registration_date, email, username, password, userpic, contacts, created_lots, placed_bets)
 VALUES
-(1, '2018-09-25 10:10:10', 'vovka_kuchkin@qmail.ru', 'Виктор Палыч', 'qwerty', NULL, 'телефон 123456', NULL, NULL),
-(2, '2018-08-17 05:41:21', 'alex999@qmail.ru', 'A L E X', 'asdfgh', NULL, 'skype alexandr223334s', NULL, NULL);
+('2018-09-25 10:10:10', 'vovka_kuchkin@qmail.ru', 'Виктор Палыч', 'qwerty', NULL, 'телефон 123456', NULL, NULL),
+('2018-08-17 05:41:21', 'alex999@qmail.ru', 'A L E X', 'asdfgh', NULL, 'skype alexandr223334s', NULL, NULL);
 
-INSERT INTO lots(datetime_start, lot_title, description, lot_picture, starting_price, current_price,
-datetime_finish, bet_increment, lot_category, lot_owner)
+INSERT INTO lots(datetime_start, title, description, picture, starting_price, current_price,
+datetime_finish, bet_increment, category, owner)
 VALUES
 ('2018-09-25 15:11:00', '2014 Rossignol District Snowboard', 'lorem ipsum', 'img/lot-1.jpg', 10999, 11500, '2018-10-25 15:11:00', 500, 1, 1),
 ('2018-09-25 15:12:00', 'DC Ply Mens 2016/2017 Snowboard', 'lorem ipsum', 'img/lot-2.jpg', 159999, 159999, '2018-10-25 15:11:00', 500, 1, 1),
@@ -22,36 +22,36 @@ VALUES
 ('2018-09-25 15:15:00', 'Куртка для сноуборда DC Mutiny Charocal', 'lorem ipsum', 'img/lot-5.jpg', 7500, 7500, '2018-10-25 15:11:00', 500, 4, 1),
 ('2018-09-25 15:16:00', 'Маска Oakley Canopy', 'lorem ipsum', 'img/lot-6.jpg', 5400, 6000, '2018-10-25 15:11:00', 500, 6, 1);
 
-INSERT INTO bets(bet_datetime, bet_sum, bet_owner, bet_lot)
+INSERT INTO bets(datetime, bet, owner, lot)
 VALUES
 ('2018-09-25 18:41:17', 11500, 2, 1),
 ('2018-09-25 18:43:17', 6000, 2, 6);
 
 /* получить все категории */
-SELECT cat_title FROM categories;
+SELECT title FROM categories;
 
 /* получить самые новые, открытые лоты. Каждый лот должен включать название,
 стартовую цену, ссылку на изображение, цену, количество ставок, название категории; */
-SELECT lots.lot_title, starting_price, current_price, lot_picture, cat_title, COUNT(bets.bet_id) as bets_quantity
-FROM lots
-LEFT JOIN categories ON cat_id = lot_category
-LEFT JOIN bets ON lots.lot_id = bets.bet_lot
+SELECT l.title, starting_price, current_price, picture, c.title, COUNT(b.id) as bets_quantity
+FROM lots l
+LEFT JOIN categories c ON c.id = category
+LEFT JOIN bets b ON l.id = b.lot
 WHERE datetime_finish > CURRENT_TIMESTAMP
-GROUP BY lot_id
+GROUP BY l.id
 ORDER BY datetime_start DESC;
 
 /* показать лот по его id. Получите также название категории, к которой принадлежит лот */
-SELECT lot_title, cat_title
-FROM lots, categories
-WHERE lot_id = 3
-AND lot_category = cat_id;
+SELECT l.title, c.title
+FROM lots l, categories c
+WHERE l.id = 3
+AND l.category = c.id;
 
 /* обновить название лота по его идентификатору */
-UPDATE lots SET lot_title = 'New Lot Title'
-WHERE lot_id = 6;
+UPDATE lots l SET l.title = 'New Lot Title'
+WHERE l.id = 6;
 
 /* получить список самых свежих ставок для лота по его идентификатору */
-SELECT bet_datetime, bet_sum, username, lot_title
-FROM bets, users, lots
-WHERE bet_lot = 1 AND bet_lot = lot_id AND bet_owner = user_id
-ORDER BY bet_datetime DESC;
+SELECT datetime, bet, username, l.title
+FROM bets b, users u, lots l
+WHERE b.lot = 1 AND b.lot = l.id AND b.owner = u.id
+ORDER BY datetime DESC;
--- schema.sql
+++ schema.sql
@@ -4,43 +4,43 @@ DEFAULT COLLATE utf8_general_ci;
 USE yeticave;
 
 CREATE TABLE categories (
-  cat_id INT AUTO_INCREMENT PRIMARY KEY,
-  cat_title VARCHAR(512) UNIQUE
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(512) UNIQUE
 );
 CREATE TABLE users (
-  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   registration_date TIMESTAMP,
   email CHAR(128) UNIQUE,
   username VARCHAR(512) UNIQUE,
   password VARCHAR(64),
-  user_pic TEXT,
+  userpic TEXT,
   contacts TEXT,
   created_lots INT,
   placed_bets INT
 );
 CREATE TABLE lots (
-  lot_id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   datetime_start TIMESTAMP,
-  lot_title VARCHAR(512),
+  title VARCHAR(512),
   description TEXT,
-  lot_picture VARCHAR(512),
+  picture VARCHAR(512),
   starting_price INT,
   current_price INT,
   datetime_finish TIMESTAMP,
   bet_increment INT,
-  lot_category INT,
-  lot_owner INT,
-  lot_winner INT,
-  FOREIGN KEY (lot_category) REFERENCES categories(cat_id),
-  FOREIGN KEY (lot_owner) REFERENCES users(user_id),
-  FOREIGN KEY (lot_winner) REFERENCES users(user_id)
+  category INT,
+  owner INT,
+  winner INT,
+  FOREIGN KEY (category) REFERENCES categories(id),
+  FOREIGN KEY (owner) REFERENCES users(id),
+  FOREIGN KEY (winner) REFERENCES users(id)
 );
 CREATE TABLE bets (
-  bet_id INT AUTO_INCREMENT PRIMARY KEY,
-  bet_datetime TIMESTAMP,
-  bet_sum INT,
-  bet_owner INT,
-  bet_lot INT,
-  FOREIGN KEY (bet_owner) REFERENCES users(user_id),
-  FOREIGN KEY (bet_lot) REFERENCES lots(lot_id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  datetime TIMESTAMP,
+  bet INT,
+  owner INT,
+  lot INT,
+  FOREIGN KEY (owner) REFERENCES users(id),
+  FOREIGN KEY (lot) REFERENCES lots(id)
 );
