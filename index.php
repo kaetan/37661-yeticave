4270ca9df69f34459d3b88f3672f88a8fd6474e5
Kirill Prokudin <kir.prokudin@yandex.ru>
2018-10-02 22:04:14 +0700

    Категории и товары из БД

--- index.php
+++ index.php
@@ -8,9 +8,39 @@ $is_auth = rand(0, 1);
 // Подключаем нужные файлы
 require_once 'functions.php';
 require_once 'data.php';
+require_once 'init.php';
 
+if (!$link) {
+    $error = mysqli_connect_error();
+    $content = include_template('error.php', ['error' => $error]);
+}
+else {
+    $sql = "SELECT id, title FROM categories ORDER BY id ASC";
+    $result = mysqli_query($link, $sql);
+
+    if ($result) {
+        $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+    else {
+        $error = mysqli_error($link);
+        $content = include_template('templates/error.php', ['error' => $error]);
+    }
+    $sql = "SELECT l.title, starting_price, current_price, picture, c.title as category, COUNT(b.id) as bets_quantity
+            FROM lots l
+            LEFT JOIN categories c ON c.id = category
+            LEFT JOIN bets b ON l.id = b.lot WHERE datetime_finish > CURRENT_TIMESTAMP
+            GROUP BY l.id
+            ORDER BY datetime_start DESC LIMIT 6";
+    if ($res = mysqli_query($link, $sql)) {
+        $lots = mysqli_fetch_all($res, MYSQLI_ASSOC);
+        $content = include_template('main.php', ['lots' => $lots, 'categories' => $categories]);
+    }
+    else {
+        $content = include_template('error.php', ['error' => mysqli_error($link)]);
+    }
+}
 // Собираем страницу и выводим ее на экран
-$page_content = include_template('index.php', ['wares' => $wares, 'categories' => $categories]);
-$layout_content = include_template('layout.php',
-    ['page_content' => $page_content, 'is_auth' => $is_auth, 'categories' => $categories, 'title' => 'Главная']);
-print($layout_content);
+$layout = include_template('layout.php',
+    ['content' => $content, 'is_auth' => $is_auth, 'categories' => $categories, 'title' => 'Главная']);
+
+print($layout);
